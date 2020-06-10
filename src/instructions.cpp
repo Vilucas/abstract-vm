ae95f87ee6da22bc0de19197ebfba1c8267b7d25
Victor LUCAS <viclucas@e1r6p2.42.fr>
2020-06-10 16:14:11 +0200

    unstable

--- src/instructions.cpp
+++ src/instructions.cpp
@@ -6,6 +6,6 @@
 void    instructions(linesManagement lm, stack Stack)
 {
     std::map<std::string, void(stack::*)(void)> tab;
-    tab.insert(std::make_pair("exit", stack::exit))
+    tab.insert(std::make_pair("exit", &stack::exit))
 
 }
--- src/instructionsParsing.cpp
+++ src/instructionsParsing.cpp
@@ -23,17 +23,22 @@ std::list<std::string>  splitString(std::string line)
     }
     return (ret);
 }
-/* pseudo code for parsing after important part
-void    checktype
-std::list<std::string> typeBoard = {
-        "int8", "int16", 
-        "int32", "float",
-        "double" };
 
+void    checkType(std::list<std::string> ret)
+{
+    std::list<std::string> typeBoard = {
+        "int8(", "int16(", 
+        "int32(", "float(",
+        "double(" };
+    
+
+    /*
     if ret[0] diff of the two isntructions thatb take param -> false
-    if ret[1] is ot a correct type -> false
+    if ret[1] is not a correct type -> false
     if whats insine the ret[1] parenthesis is bullshit -> false
-*/
+    */
+}
+
 std::list<std::string>     slicingBySpaces(linesManagement lm)
 {
     std::list<std::string> instructionBoard = { "push", "pop",
@@ -42,13 +47,11 @@ std::list<std::string>     slicingBySpaces(linesManagement lm)
         "mul", "div",
         "mod", "print",
         "exit" };
-
+    
     std::cout << "line = '" << lm.line << "'" << std::endl;
     std::list<std::string> ret = splitString(lm.line);
     std::list<std::string>::iterator it = ret.begin();
 
-  //  if (ret.size() > 1)
-//     CheckTypes(lm, ret)
     if (ret.size() > 2)
         throw(LexicalErrorException(std::to_string(lm.line_count)));
     bool found;
